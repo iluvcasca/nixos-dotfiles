9e0d109203a268853103ce17130dcab1d828aced
iluvcasca <sntcillian@gmail.com>
2023-12-23 13:21:59 +0100

    update

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Home Manager configuration of iluvgriffith";
+  description = "Home Manager configuration of kgriset";
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
@@ -9,7 +9,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     # source of Hyprland
-    hyprland.url = "github:hyprwm/Hyprland";
+    #hyprland.url = "github:hyprwm/Hyprland";
     nix-colors.url = "github:misterio77/nix-colors";
     prism.url = "github:IogaMaster/prism";
 
@@ -19,29 +19,29 @@
     plugin-betterTerm-nvim.url = "github:CRAG666/betterTerm.nvim";
     plugin-betterTerm-nvim.flake = false;
 
-    plugin-header_42-vim.url = "github:42Paris/42header";
-    plugin-header_42-vim.flake = false;
-
     emacs-overlay.url = "github:nix-community/emacs-overlay";
+    plugin-header_42_vim.url = "github:42Paris/42header";
+    plugin-header_42_vim.flake = false;
 
+    plugin-norminette-vim.url = "github:alexandregv/norminette-vim";
+    plugin-norminette-vim.flake = false;
   };
 
-  outputs = { self, nixpkgs, home-manager, hyprland, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      homeConfigurations."iluvgriffith" =
-        home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
+      homeConfigurations."kgriset" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
 
-          # Specify your home configuration modules here, for example,
-          # the path to your home.nix.
-          modules = [ ./home.nix hyprland.homeManagerModules.default ];
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
 
-          # Optionally use extraSpecialArgs
-          # to pass through arguments to home.nix
-          extraSpecialArgs = { inherit inputs; };
-        };
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+        extraSpecialArgs = { inherit inputs; };
+      };
     };
 }
